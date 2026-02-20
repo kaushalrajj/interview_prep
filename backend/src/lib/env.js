429168c349a4a97e1348085fc3654fffc8416336
kaushalrajj <kaushalrajpandey1122@gmail.com>
2026-02-20 23:01:33 +0530

    gallery updated

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -4,4 +4,5 @@ dotenv.config();
 export const ENV = {
     PORT: process.env.PORT || 3000,
     DB_URL: process.env.DB_URL,
+    NODE_ENV: process.env.NODE_ENV || "development"
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,26 @@ console.log("starting server....");
 
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { ENV } from "./lib/env.js";
 
 dotenv.config();
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.get("/health",(req,res)=>{
     res.status(200).json({message:"okay apis working herre babe "})
 })
 
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+});
+}
 const PORT = ENV.PORT;
 app.listen(PORT,()=>{
     console.log(`server is running on the port ${ENV.PORT}`);
