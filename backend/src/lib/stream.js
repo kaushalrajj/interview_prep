0f348afa433058ddfa4fb41c07782d91e8f3d34c
kaushalrajj <kaushalrajpandey1122@gmail.com>
2026-03-13 10:35:03 +0530

    csr done

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,33 +1,34 @@
-import {StreamChat} from "stream-chat";
-import {StreamClient} from "@stream-io/node-sdk";
+import { StreamChat } from "stream-chat";
+import { StreamClient } from "@stream-io/node-sdk";
 import { ENV } from "./env.js";
 
-const apiKey = ENV.STREAM_API_KEY
-const apiSecret = ENV.STREAM_API_SECRET
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
 
-if(!apiKey || !apiSecret){
-    console.error("stream APi key or secret dont exist")
+if (!apiKey || !apiSecret) {
+  console.error("Stream API key or secret missing");
+  process.exit(1);
 }
 
-export const chatClient = StreamChat.getInstance(apiKey,apiSecret)
-export const streamClient = new StreamClient(apiKey,apiSecret);
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+export const streamClient = new StreamClient(apiKey, apiSecret);
 
-export const upsertStreamUser = async(userData)=>{
-    try{
-        await chatClient.upsertUser(userData)
-        console.log("stream user upserted succesfully",userData)
-    }
-    catch(error){
-        console.error("error upserting stream user",error)
-    }
-}
+export const upsertStreamUser = async (userData) => {
+  try {
+    await chatClient.upsertUser(userData);
+    console.log("stream user upserted successfully", userData);
+  } catch (error) {
+    console.error("error upserting stream user", error);
+  }
+};
 
-export const deleteStreamUser = async(userId)=>{
-    try{
-        await chatClient.deleteUser(userId)
-        console.log("stream user deleted succesfully :",userId);
-    }
-    catch(error){
-        console.error("error deleting stream user",error)
-    }
-}
+export const deleteStreamUser = async (userId) => {
+  try {
+    await chatClient.deleteUser(userId, {
+      hard_delete: true,
+    });
+    console.log("stream user deleted successfully :", userId);
+  } catch (error) {
+    console.error("error deleting stream user", error);
+  }
+};
